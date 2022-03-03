f5774d52b0fb830db4052e7d57a8dce92a69f25e
adkatarine <adllakatarine@hotmail.com>
2022-03-03 20:55:41 -0300

    feat: Formata dados do episodio

--- src/api_colector/api_colector.py
+++ src/api_colector/api_colector.py
@@ -1,11 +1,20 @@
 from src.infra.api_consumer import RickMortyApiConsumer
-from typing import Type
 
 
 class RickMortyApiColector:
-    def __init__(self, rm_api_consumer: Type[RickMortyApiConsumer]) -> None:
-        self.rm_api_consumer = rm_api_consumer
+    def __init__(self) -> None:
+        self.rm_api_consumer = RickMortyApiConsumer()
 
     def info_episode(self, id_episode: int) -> dict:
         rm_api_response = self.rm_api_consumer.get_episode(id_episode)
-        return rm_api_response
+        return self.format_info_episode(rm_api_response)
+
+    def format_info_episode(self, episode: dict):
+        formated_ep = {
+            "id": episode.response["id"],
+            "name": episode.response["name"],
+            "episode": episode.response["episode"],
+            "air_date": episode.response["air_date"],
+            "characters": episode.response["characters"],
+        }
+        return formated_ep
